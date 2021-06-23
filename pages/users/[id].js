76b33dc26b8d61d08aeb561f4c6a2b6017693a4f
justinsalasdev <justinsalasdev@gmail.com>
2021-06-23 21:54:56 +0800

    .

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -29,7 +29,7 @@ export async function getStaticProps(context) {
     matched.push({ account: doc.id, ...doc.data() })
   );
 
-  return { props: { userId, ...matched[0] }, revalidate: 1 };
+  return { props: { userId, ...matched[0] } };
 }
 
 export async function getStaticPaths() {
--- pages/users/index.js
+++ pages/users/index.js
@@ -21,6 +21,6 @@ export async function getStaticProps() {
   return {
     props: {
       users: users
-    } // will be passed to the page component as props
+    }
   };
 }
